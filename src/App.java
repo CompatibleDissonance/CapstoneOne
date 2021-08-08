14572896a3bd3ce814b2c1aa3654bc1f47cfa881
CompatibleDissonance <cnlynch20@gmail.com>
2021-08-08 19:53:58 +1000

    added basic card payment input.

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class App {
     static boolean ordering = true;
 
-    public static void done (double total) {
+    public static void done(double total) {
         boolean ordering = false;
 
         System.out.println("Debug Deez nuts");
@@ -29,25 +29,48 @@ public class App {
         System.out.println("Please input the item number you wish to purchase: ");
         int purchaseInput = input.nextInt();
 
-            switch (purchaseInput) {
-                case 1:
-                 System.out.println(Products.Glitches.getSongName() + " added to cart");
-                 break;
+        switch (purchaseInput) {
+            case 1:
+                System.out.println(Products.Glitches.getSongName() + " added to cart");
+                break;
 
-                case 2:
-                    System.out.println(Products.NYCompression.getSongName() + " added to cart");
-                    break;
+            case 2:
+                System.out.println(Products.NYCompression.getSongName() + " added to cart");
+                break;
 
-                case 3:
-                    System.out.println(Products.BadMood.getSongName() + " added to cart");
-                    break;
+            case 3:
+                System.out.println(Products.BadMood.getSongName() + " added to cart");
+                break;
 
-                case 4:
-                    System.out.println(Products.Sleuth.getSongName() + " added to cart");
+            case 4:
+                System.out.println(Products.Sleuth.getSongName() + " added to cart");
 
-                default:
-                    System.out.println(purchaseInput + " " + "Is an invalid selection");
-            }
-
+            default:
+                System.out.println(purchaseInput + " " + "Is an invalid selection");
         }
+
+
+        System.out.println("please enter your email: ");
+
+        String emailInput = input.next();
+        System.out.println("Your order will be delivered to: " + emailInput);
+
+        System.out.println("Please enter the following payment details!");
+
+        System.out.println("Name on card: ");
+        String cardName = input.next();
+
+        System.out.println("Card number: ");
+        Long cardNumber = input.nextLong();
+
+        System.out.println("Card expiry date: ");
+        int cardExpDate = input.nextInt();
+
+        System.out.println("Card security code (3(Three) digits on the back): ");
+        int cardSecCode = input.nextInt();
+
+        System.out.println("Thank you for your purchase! please check your email (" + emailInput + ") for your download link!");
+
+    }
+
 }
